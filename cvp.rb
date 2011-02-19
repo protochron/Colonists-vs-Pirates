bc340795414ac261a3542436f0dcea6134849c94
Daniel Norris <dnorris10@gmail.com>
2011-02-19 15:42:21 -0800

    Adds GameObject class, starts main game code

---
+++ cvp.rb
@@ -0,0 +1,4 @@
+# This is the main driver. Code will follow shortly.
+#
+
+require 'gosu'
---
+++ lib/game_object.rb
@@ -0,0 +1,4 @@
+# Implements basic attributes common between all game objects.
+# Game objects are all objects (other than GUI components) that the player interacts with.
+class GameObject
+end
