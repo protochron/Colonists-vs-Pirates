a7fb29b4aad5a511aeaba8fa755844e2c10c1c24
Codiferous <codymiller@vandals.uidaho.edu>
2011-02-19 17:49:03 -0800

    Added mouse_move, button_clicked, and button_unclicked methos to GameWindow. Introduced a class variable called @ui that maintains a list of UI elements to draw.
    Modified the mouse cursor to start at (0,0) and to
    always be visible
    
    Transformed some simple UI graphics to Buttons.
    
    Changed the default behavior of Buttons and added
    a draw command, so they can draw themselves.

--- lib/button.rb
+++ lib/button.rb
@@ -7,24 +7,38 @@ require_relative "clickable"
 class Button
   include Clickable
   
+  attr_accessor :image
+  
+  def initialize(ix, iy, iz, image)
+    @image = image
+    @image_x, @image_y, @image_z = ix, iy, iz
+    
+    self.clickable_area(ix, iy, ix + image.width, iy + image.height)
+  end
+  
+  def draw()
+    @image.draw(@image_x, @image_y, @image_z)
+  end
+  
   # Called whenever the mouse is clicked within the area of this object
   def clicked(e)
-    puts "Clicked at position #{e.x, e.y}"
+    puts "Clicked!!!"
+    
   end
   
   # Called whenever the mouse button is released within the area of this object
   def unclicked(e)
-    puts "Unclicked at position #{e.x, e.y}"
+    puts "Unclicked!"
   end
   
   # Called whenever the mouse moves into the clickable region of this object
   def mouse_in(e)
-    puts "Moused moved in!"
+    puts "Mouse moved in!"
   end
   
   # Called whenever the mouse moves out of the clickable region of this object
   def mouse_out(e)
-    puts "Moused moved out!"
+    puts "Mouse moved out!"
   end
   
 end
