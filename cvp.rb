956dfff39c696c70f840f5ce84c27af88153284f
Codiferous <codymiller@vandals.uidaho.edu>
2011-02-19 18:11:50 -0800

    Removed all the user interface stuff from the GameWindow class to the new UserInterface class.

--- cvp.rb
+++ cvp.rb
@@ -5,6 +5,7 @@ require 'gosu'
 require 'optparse'
 require './lib/ship'
 require './lib/button'
+require './lib/user_interface'
 
 module ZOrder
     Background, UI, Enemy, Player = *0..3
@@ -12,8 +13,12 @@ end
 
 $window_x = 800
 $window_y = 600
+
 class GameWindow < Gosu::Window
+    include UserInterface
+    
     attr_reader :cannon_ball
+    
     def initialize
         super($window_x,$window_y, false)
         self.caption = "Colonists vs. Pirates!"
@@ -24,15 +29,9 @@ class GameWindow < Gosu::Window
         @background = Gosu::Image.new(self, "images/background.png")
         @ship = Gosu::Image.new(self, "images/fast_boat.png")
         @money_bar = Gosu::Image.new(self, "images/money_amount.png")
-        exit = Gosu::Image.new(self, "images/exit.png")
         @cannon_fire = Gosu::Image.new(self, "images/canon_fire.png")
         @cannon_reg = Gosu::Image.new(self, "images/canon_reg.png")
-        purchase = Gosu::Image.new(self, "images/purchase.png")
 
-        #@exit_b = Button.new(600, 0, ZOrder::Background, exit)
-        #@exit_b.clickable_area(600, 0, 650, 30)
-        @ui = Button.new(725, 0, ZOrder::Background, exit),
-              Button.new(675, 0, ZOrder::Background, purchase)
         @cannon_ball = Gosu::Image.new(self, "images/cannon_ball.png")
 
         # Object collections
@@ -49,68 +48,26 @@ class GameWindow < Gosu::Window
             close
         end
 
-        if mouse_x != @mouse_pos_x or mouse_y != @mouse_pos_y then
-          mouse_move
-        end
+        mouse_update
         
         @ships.each{|s| s.tick}
-    end
-
-    def mouse_move
-      event = MouseEvent.new mouse_x, mouse_y, nil
-      
-      @ui.each do |elem|
-        unless elem.within_clickable?(@mouse_pos_x, @mouse_pos_y)
-          if elem.within_clickable?(event.x, event.y)
-            elem.mouse_in(event)
-          end
-        end
-        
-        if elem.within_clickable?(@mouse_pos_x, @mouse_pos_y)
-          unless elem.within_clickable?(event.x, event.y)
-            elem.mouse_out(event)
-          end
-        end
-      end
-      
-      @mouse_pos_x, @mouse_pos_y = event.x, event.y
     end
     
     def needs_cursor?
       return true
     end
 
-    def button_down(id)
-      event = MouseEvent.new mouse_x, mouse_y, id
-      
-      @ui.each do |elem|
-        elem.clicked(event) if elem.within_clickable?(event.x, event.y)
-      end
-    end
-    
-    def button_up(id)
-      event = MouseEvent.new mouse_x, mouse_y, id
-      
-      @ui.each do |elem|
-        elem.unclicked(event) if elem.within_clickable?(event.x, event.y)
-      end
-    end
-
-
     def draw
         #Background and UI draw
         @background.draw(0,0, ZOrder::Background, 1.0, 1.0)
-
-        @ui.each do |elem|
-          elem.draw
-        end
-
 
         @money_bar.draw(10, 0, ZOrder::Background, 1.0, 1.0)
         #@purchase.draw(500, 0, ZOrder::Background, 1.0, 1.0)
         #@exit.draw(600, 0, ZOrder::Background, 1.0, 1.0)
         @cannon_fire.draw(40, 500, ZOrder::Background, 1.0, 1.0)
         @cannon_reg.draw(120, 500, ZOrder::Background, 1.0, 1.0)
+
+        draw_gui
 
         # Call individual object draw methods
         @ships.each{ |s| s.image.draw(s.x, s.y, ZOrder::Enemy, 1.0,1.0) }
---
+++ lib/user_interface.rb
@@ -0,0 +1,68 @@
+# This is the module that provides all the UserInterface methods.
+# Its main purpose is to keep the GameWindow class as clean as possible.
+
+module UserInterface
+
+  def initialize(*params)
+    super(*params)
+    
+    @mouse_pos_x, @mouse_pos_y = 0,0
+
+    exit = Gosu::Image.new(self, "images/exit.png")
+    purchase = Gosu::Image.new(self, "images/purchase.png")
+
+    @ui = Button.new(725, 0, ZOrder::Background, exit),
+          Button.new(675, 0, ZOrder::Background, purchase)
+
+  end
+  
+  def mouse_update
+    if mouse_x != @mouse_pos_x or mouse_y != @mouse_pos_y then
+      mouse_move
+    end
+  end
+
+  def mouse_move
+    event = MouseEvent.new mouse_x, mouse_y, nil
+    
+    @ui.each do |elem|
+      unless elem.within_clickable?(@mouse_pos_x, @mouse_pos_y)
+        if elem.within_clickable?(event.x, event.y)
+          elem.mouse_in(event)
+        end
+      end
+      
+      if elem.within_clickable?(@mouse_pos_x, @mouse_pos_y)
+        unless elem.within_clickable?(event.x, event.y)
+          elem.mouse_out(event)
+        end
+      end
+    end
+    
+    @mouse_pos_x, @mouse_pos_y = event.x, event.y
+  end
+
+
+  def button_down(id)
+    event = MouseEvent.new mouse_x, mouse_y, id
+    
+    @ui.each do |elem|
+      elem.clicked(event) if elem.within_clickable?(event.x, event.y)
+    end
+  end
+  
+  def button_up(id)
+    event = MouseEvent.new mouse_x, mouse_y, id
+    
+    @ui.each do |elem|
+      elem.unclicked(event) if elem.within_clickable?(event.x, event.y)
+    end
+  end
+
+  def draw_gui
+    @ui.each do |elem|
+      elem.draw
+    end
+  end
+  
+end
