2f0ffa3e2a12977a7886650e7c216bdf419b2153
Daniel Norris <dnorris10@gmail.com>
2011-02-19 16:34:21 -0800

    Draws background, adds tick method to GameObject

--- cvp.rb
+++ cvp.rb
@@ -4,18 +4,24 @@
 require 'gosu'
 require 'optparse'
 
+module ZOrder
+    Background, UI, Enemy, Player = *0..3
+end
+
 
 class GameWindow < Gosu::Window
     def initialize
         super(800,600, false)
         self.caption = "Colonists vs. Pirates!"
         @font = Gosu::Font.new(self, Gosu::default_font_name, 10)
+        @background = Gosu::Image.new(self, "images/background.png")
     end
 
     def update
     end
 
     def draw
+        @background.draw(0,0, ZOrder::Background, 1.0, 1.0)
     end
 end
 
