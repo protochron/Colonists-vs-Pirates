de0c1baac65a4bba095f3c0de668ead4310cc46d
Daniel Norris <dnorris10@gmail.com>
2011-02-19 16:00:00 -0800

    Starts writing main game loop

--- cvp.rb
+++ cvp.rb
@@ -2,3 +2,20 @@
 #
 
 require 'gosu'
+require 'optparse'
+
+
+class GameWindow < Gosu::Window
+    def initialize
+    end
+
+    def update
+    end
+
+    def draw
+    end
+end
+
+# Actully draw our window
+window = GameWindow.new
+window.show
